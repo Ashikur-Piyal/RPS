fffaff24e4ac39afdbe17d7755d1b5c4b8a11b08
Ashikur Rahman Piyal <piyal01780@gmail.com>
2024-05-15 22:07:15 +0600

    pushing word

--- RPS.js
+++ RPS.js
@@ -22,7 +22,7 @@ function autoplay(){
 
 if (!isAutoPlaying){
   
-  intervalId = setInterval(function(){
+  intervalId = setInterval(() => {
   const playerMove = pickComputerMove();
   playGame(playerMove);
   },1000)
@@ -45,18 +45,49 @@ buttonName.classList.add('stopButton')
 }
 } 
 
+document.querySelector('.js-rock-button').addEventListener('click', ()=> {playGame('rock')});
+
+document.querySelector('.js-paper-button').addEventListener('click', ()=> {playGame('paper')});
+
+document.querySelector('.js-scissors-button').addEventListener('click', ()=> {playGame('scissors')});
+
+function resetScore(){
+score.wins =0;
+score.losses=0;
+score.ties=0;
+localStorage.removeItem('score');
+updateScoreElement();
+}
+document.querySelector('.reset-button').addEventListener('click', resetScore);
+
+document.querySelector('.auto-play').addEventListener('click', autoplay);
+
+document.body.addEventListener('keydown',(event)=>{
+if(event.key==='r'){
+  playGame('rock');
+}
+else if(event.key === 'p'){
+  playGame('paper');
+
+}else if(event.key==='s'){
+  playGame('scissors');
+}
+})
+
+
+
 function playGame(playerMove){
 
 const computerMove = pickComputerMove(); 
 pickComputerMove();
 let result = '';
 
-if (playerMove=== 'scissor'){
+if (playerMove=== 'scissors'){
 if(computerMove === 'rock'){
 result ='You lose.';
 } else if(computerMove === 'paper'){
 result ='You win.';
-} else if(computerMove === 'scissor'){
+} else if(computerMove === 'scissors'){
 result ='Tie.';
 }
 
@@ -65,7 +96,7 @@ if(computerMove === 'rock'){
 result ='You win.';
 } else if(computerMove === 'paper'){
 result ='Tie.';
-} else if(computerMove === 'scissor'){
+} else if(computerMove === 'scissors'){
 result ='You lose.';
 }
 
@@ -74,7 +105,7 @@ if(computerMove === 'rock'){
 result ='Tie.';
 } else if(computerMove === 'paper'){
 result ='You lose.';
-} else if(computerMove === 'scissor'){
+} else if(computerMove === 'scissors'){
 result ='You win.';
 }
 
@@ -100,6 +131,7 @@ document.querySelector('.js-moves').innerHTML = `You <img src="images/${playerMo
 
 }
 
+
 function updateScoreElement(){
 document.querySelector('.js-score').innerHTML= `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 
@@ -115,7 +147,7 @@ computerMove = 'rock';
 }else if(randomNumber >= 1/3 && randomNumber <2/3){
 computerMove ='paper';
 } else if (randomNumber >= 2/3 && randomNumber <1){
-computerMove = 'scissor';
+computerMove = 'scissors';
 }
 
 return computerMove;
